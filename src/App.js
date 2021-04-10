e66591657dc8d206fee3b19d2e860981a0448dfc
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-04-10 13:19:27 +0530

    homepage created and made responsive

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Layout from './components/Layout/Layout';
 
+import {BrowserRouter} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <Layout />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Layout />
+      </div>
+    </BrowserRouter>
   );
 }
 
