c4ab6b5fb9a4449cd3d55dff91f839cc215bd3b8
pratik-276 <pratikmukherjee32@gmail.com>
2021-04-11 07:12:13 +0530

    home image made responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,13 +3,13 @@ import classes from './Home.module.css';
 
 const Home = () => {
     return (
-        <div class="container valign-wrapper" style={{width: "90%", minHeight: "80vh"}}>
-            <div class="row">
-                <div class="col s12 m5 l6">
+        <div className="container valign-wrapper" style={{width: "90%", minHeight: "80vh"}}>
+            <div className="row">
+                <div className="col s12 m5 l6">
                     <h1 className={classes.headerText}>Manage your daily tasks more effectively</h1>
                 </div>
-                <div class="col s12 m7 l6">
-                    <img src="/tasks.svg" width="100%" />
+                <div className="col s12 m7 l6">
+                    <img src="/tasks.svg" className="responsive-img" alt="headImg" />
                 </div>
             </div>
         </div>
