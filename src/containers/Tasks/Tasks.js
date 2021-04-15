7ac10888b71e5202b8fce594da5113b2d8f9a239
Pratik Mukherjee <pratikmukherjee32@gmail.com>
2021-04-15 11:09:38 +0530

    task update validtion added

--- src/containers/Tasks/Tasks.js
+++ src/containers/Tasks/Tasks.js
@@ -4,16 +4,33 @@ import M from 'materialize-css/dist/js/materialize.min.js';
 
 import {connect} from 'react-redux';
 import * as actions from '../../store/actions/index';
+import { taskValidator } from '../../utility/validators';
 
 class Tasks extends Component {
     state = {
         id: "",
-        title: "",
-        description: "",
-        category: "",
-        date: "",
-        time: "",
-        formState: true
+        title: {
+            value: "",
+            label: ""
+        },
+        description: {
+            value: "",
+            label: ""
+        },
+        category: {
+            value: "task",
+            label: "active"
+        },
+        date: {
+            value: "",
+            label: ""
+        },
+        time: {
+            value: "",
+            label: ""
+        },
+        formState: true,
+        valid: false
     }
     componentWillMount(){
         this.props.getTasks();
@@ -33,47 +50,124 @@ class Tasks extends Component {
         document.getElementById("addtask").style.display = "none";
         this.setState({
             id: "",
-            title: "",
-            description: "",
-            date: "",
-            time: "",
-            category: "",
-            formState: true
+            title: {
+                value: "",
+                label: ""
+            },
+            description: {
+                value: "",
+                label: ""
+            },
+            category: {
+                value: "task",
+                label: "active"
+            },
+            date: {
+                value: "",
+                label: ""
+            },
+            time: {
+                value: "",
+                label: ""
+            },
+            formState: true,
+            valid: false
         });
     }
     onTitleChange = (event) => {
-        this.setState({title: event.target.value});
+        const newState = {
+            ...this.state,
+            title: {
+                value: event.target.value,
+                label: "active"
+            }
+        };
+        newState.valid = taskValidator(newState);
+        this.setState(newState);
     }
     onDescChange = (event) => {
-        this.setState({description: event.target.value});
+        const newState = {
+            ...this.state,
+            description: {
+                value: event.target.value,
+                label: "active"
+            }
+        };
+        this.setState(newState);
     }
     onCategoryChange = (event) => {
-        this.setState({category: event.target.value});
+        const newState = {
+            ...this.state,
+            category: {
+                value: event.target.value,
+                label: "active"
+            }
+        };
+        this.setState(newState);
     }
     onDateChange = () => {
-        this.setState({date: document.getElementById("date").value});
+        const newState = {
+            ...this.state,
+            date: {
+                value: document.getElementById("date").value,
+                label: "active"
+            }
+        };
+        newState.valid = taskValidator(newState);
+        this.setState(newState);
     }
     onTimeChange = () => {
-        this.setState({time: document.getElementById("time").value});
+        const newState = {
+            ...this.state,
+            time: {
+                value: document.getElementById("time").value,
+                label: "active"
+            }
+        };
+        this.setState(newState);
     }
     onFormSubmit = (event) => {
         event.preventDefault();
+        const data = {
+            id: this.state.id,
+            title: this.state.title.value,
+            description: this.state.description.value,
+            category: this.state.category.value,
+            date: this.state.date.value,
+            time: this.state.time.value
+        }
         if(this.state.formState){
-            this.props.addTask(this.state);
+            this.props.addTask(data);
         }else{
-            this.props.editTask(this.state);
+            this.props.editTask(data);
         }
         this.clearForm();
     }
     onEditTask = (data) => {
         this.setState({
             id: data.id,
-            category: data.category,
-            title: data.title,
-            description: data.description,
-            date: data.date,
-            time: data.time,
-            formState: false
+            category: {
+                value: data.category,
+                label: "active"
+            },
+            title: {
+                value: data.title,
+                label: "active"
+            },
+            description: {
+                value: data.description,
+                label: "active"
+            },
+            date: {
+                value: data.date,
+                label: "active"
+            },
+            time: {
+                value: data.time,
+                label: "active"
+            },
+            formState: false,
+            valid: true
         });
         document.getElementById("addtask").style.display = "block";
     }
@@ -104,24 +198,27 @@ class Tasks extends Component {
                 }}>
                     <form>
                         <div className="input-field">
-                            <input type="text" id="title" value={this.state.title}
-                                onChange={this.onTitleChange} placeholder="Title" />
+                            <input type="text" id="title" value={this.state.title.value}
+                                onChange={this.onTitleChange} />
+                            <label htmlFor="title" className={this.state.title.label}>Title</label>
                         </div>
                         <div className="input-field">
-                            <textarea className="materialize-textarea" id="description" cols="30" rows="10" value={this.state.description}
-                            onChange={this.onDescChange} placeholder="Description (optional)"></textarea>
+                            <textarea className="materialize-textarea" id="description" cols="30" rows="10" value={this.state.description.value}
+                            onChange={this.onDescChange}></textarea>
+                            <label htmlFor="description" className={this.state.description.label}>Description (optional)</label>
                         </div>
                         <div className="input-field">
                             <input type="text" className="datepicker" id="date"
-                                value={this.state.date} placeholder="Choose a Date" />
+                                value={this.state.date.value} />
+                            <label htmlFor="date" className={this.state.date.label}>Choose a Date</label>
                         </div>
                         <div className="input-field">
                             <input type="text" className="timepicker" id="time"
-                            value={this.state.time} placeholder="Choose a Time (optional)" />
+                            value={this.state.time.value} />
+                            <label htmlFor="time" className={this.state.time.label}>Choose a Time (optional)</label>
                         </div>
                         <div className="input-field">
-                            <select className="browser-default" id="category" value={this.state.category} onChange={this.onCategoryChange}>
-                                <option value="" disabled selected> Select a category</option>
+                            <select className="browser-default" id="category" value={this.state.category.value} onChange={this.onCategoryChange}>
                                 <option value="task">Task</option>
                                 <option value="quit">Quit a Bad Habit</option>
                                 <option value="meditation">Meditation</option>
@@ -130,13 +227,16 @@ class Tasks extends Component {
                                 <option value="health">Health</option>
                                 <option value="exercise">Exercise</option>
                             </select>
+                            <label htmlFor="category" className={this.state.category.label}>Select a category</label>
                         </div>
                     </form>
-                    <a className="btn green white-text waves-effect waves-red" onClick={this.onFormSubmit}>{this.state.formState?"Add":"Update"}</a>&nbsp;
+                    <a className="btn green white-text waves-effect waves-red" 
+                        onClick={this.onFormSubmit}
+                        disabled={!this.state.valid}>
+                            {this.state.formState?"Add":"Update"}</a>&nbsp;
                     <a class="btn red white-text waves-effect waves-light" 
                         onClick={() => this.clearForm()}>Cancel</a>
                 </div>
-                <div style={{marginTop: "20px"}}></div>
             </div>
         );
     }
--- src/utility/validators.js
+++ src/utility/validators.js
@@ -4,3 +4,11 @@ export const habitValidator = (title) => {
     isValid = isValid && title.length<50;
     return isValid;
 }
+
+export const taskValidator = (task) => {
+    let isValid = true;
+    isValid = isValid && task.title.value.length>1;
+    isValid = isValid && task.title.value.length<50;
+    isValid = isValid && task.date.value!=="";
+    return isValid;
+}
